4f2f9399e85f4ebfe96655fb5057f6e43e1e4fdc
unknown <sarthakgupta1376@gmail.com>
2024-06-20 13:39:19 +0530

    project added

---
+++ client/src/firebase.js
@@ -0,0 +1,37 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-blog-b327f.firebaseapp.com',
+//   projectId: 'mern-blog-b327f',
+//   storageBucket: 'mern-blog-b327f.appspot.com',
+//   messagingSenderId: '699397991367',
+//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHhcT5fSAD_PTUitxD4W0Ou5kQOQaGO3k",
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "blogs-7b51a.firebaseapp.com",
+  projectId: "blogs-7b51a",
+  storageBucket: "blogs-7b51a.appspot.com",
+  messagingSenderId: "1028698045815",
+  appId: "1:1028698045815:web:4b7f902783698d73273572"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
